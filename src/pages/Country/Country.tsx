bd02f211882929354e1121d0029152a26a9bb757
GreenXred <antonzo09@mail.ru>
2025-11-24 03:01:10 +0800

    useParams Country.tsx to get the country code from the URL and display it.

--- src/pages/Country/Country.tsx
+++ src/pages/Country/Country.tsx
@@ -1,8 +1,12 @@
+import { useParams } from "react-router-dom";
+
 export default function Country() {
+    const { code } = useParams();
+
     return (
         <div className="flex items-center justify-center">
-            <h1 className="text-3xl font-bold text-emerald-300">
-                Country
+            <h1 className="text-3xl text-emerald-300 font-bold">
+                Страна: {code}
             </h1>
         </div>
     );
