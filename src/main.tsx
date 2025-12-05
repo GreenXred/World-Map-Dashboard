2e059c310967e749c45f733d405f3cd212bc03d1
GreenXred <antonzo09@mail.ru>
2025-12-06 05:46:24 +0800

    Удалены файлы и строчки кода старой локализации, так как теперь используется инструмент i18next

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,6 @@ import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./i18next.ts";
 
-
-import { LanguageProvider } from "./Localization/LanguageContext.tsx";
 import { store } from "./store/Store.ts";
 
 import './index.css'
@@ -16,14 +14,12 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LanguageProvider>
-      <Provider store={store}>
-        <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </QueryClientProvider>
-      </Provider>
-    </LanguageProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </Provider>
   </StrictMode>
 )
